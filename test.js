530de98d74c6d0a62a1f393070d5290710c9a536
sudarshan bairagi <sudarshan.bairagi@taggbox.com>
2024-06-11 19:10:44 +0530

    update

---
+++ test.js
@@ -0,0 +1,402 @@
+{
+"id": 8748525977856,
+"title": "7 Shakra Bracelet",
+"handle": "chain-bracelet",
+"description": "<p>7 chakra bracelet, in blue or black.</p>",
+"published_at": "2024-06-10T11:19:48+05:30",
+"created_at": "2024-06-10T11:19:51+05:30",
+"vendor": "Company 123",
+"type": "Bracelet",
+"tags": [
+"Beads"
+],
+"price": 4299,
+"price_min": 4299,
+"price_max": 4299,
+"available": true,
+"price_varies": false,
+"compare_at_price": 4499,
+"compare_at_price_min": 4499,
+"compare_at_price_max": 4499,
+"compare_at_price_varies": false,
+"variants": [
+{
+"id": 45376839287040,
+"title": "Rubber / Blue / L",
+"option1": "Rubber",
+"option2": "Blue",
+"option3": "L",
+"sku": null,
+"requires_shipping": true,
+"taxable": true,
+"featured_image": {
+"id": 41166146142464,
+"product_id": 8748525977856,
+"position": 2,
+"created_at": "2024-06-10T11:19:52+05:30",
+"updated_at": "2024-06-10T11:19:52+05:30",
+"alt": null,
+"width": 925,
+"height": 618,
+"src": "https://cdn.shopify.com/s/files/1/0707/1354/0864/files/navy-blue-chakra-bracelet_925x_8b2d20e0-f381-43d4-a5fb-65e9095877e5.jpg?v=1717998592",
+"variant_ids": [
+45376839287040
+]
+},
+"available": true,
+"name": "7 Shakra Bracelet - Rubber / Blue / L",
+"public_title": "Rubber / Blue / L",
+"options": [
+"Rubber",
+"Blue",
+"L"
+],
+"price": 4299,
+"weight": 0,
+"compare_at_price": 4499,
+"inventory_management": null,
+"barcode": null,
+"featured_media": {
+"alt": null,
+"id": 33953370210560,
+"position": 2,
+"preview_image": {
+"aspect_ratio": 1.497,
+"height": 618,
+"width": 925,
+"src": "https://cdn.shopify.com/s/files/1/0707/1354/0864/files/navy-blue-chakra-bracelet_925x_8b2d20e0-f381-43d4-a5fb-65e9095877e5.jpg?v=1717998592"
+}
+},
+"requires_selling_plan": false,
+"selling_plan_allocations": []
+},
+{
+"id": 45387608719616,
+"title": "Rubber / Blue / XL",
+"option1": "Rubber",
+"option2": "Blue",
+"option3": "XL",
+"sku": "",
+"requires_shipping": true,
+"taxable": true,
+"featured_image": null,
+"available": true,
+"name": "7 Shakra Bracelet - Rubber / Blue / XL",
+"public_title": "Rubber / Blue / XL",
+"options": [
+"Rubber",
+"Blue",
+"XL"
+],
+"price": 4299,
+"weight": 0,
+"compare_at_price": 4499,
+"inventory_management": null,
+"barcode": "",
+"requires_selling_plan": false,
+"selling_plan_allocations": []
+},
+{
+"id": 45376839319808,
+"title": "Rubber / Black / L",
+"option1": "Rubber",
+"option2": "Black",
+"option3": "L",
+"sku": null,
+"requires_shipping": true,
+"taxable": true,
+"featured_image": {
+"id": 41166146109696,
+"product_id": 8748525977856,
+"position": 1,
+"created_at": "2024-06-10T11:19:52+05:30",
+"updated_at": "2024-06-10T11:19:52+05:30",
+"alt": null,
+"width": 925,
+"height": 1386,
+"src": "https://cdn.shopify.com/s/files/1/0707/1354/0864/files/7-chakra-bracelet_925x_f6b9d552-6950-4b08-87bf-82fd4f0d3641.jpg?v=1717998592",
+"variant_ids": [
+45376839319808
+]
+},
+"available": true,
+"name": "7 Shakra Bracelet - Rubber / Black / L",
+"public_title": "Rubber / Black / L",
+"options": [
+"Rubber",
+"Black",
+"L"
+],
+"price": 4299,
+"weight": 0,
+"compare_at_price": 4499,
+"inventory_management": null,
+"barcode": null,
+"featured_media": {
+"alt": null,
+"id": 33953370177792,
+"position": 1,
+"preview_image": {
+"aspect_ratio": 0.667,
+"height": 1386,
+"width": 925,
+"src": "https://cdn.shopify.com/s/files/1/0707/1354/0864/files/7-chakra-bracelet_925x_f6b9d552-6950-4b08-87bf-82fd4f0d3641.jpg?v=1717998592"
+}
+},
+"requires_selling_plan": false,
+"selling_plan_allocations": []
+},
+{
+"id": 45387608752384,
+"title": "Rubber / Black / XL",
+"option1": "Rubber",
+"option2": "Black",
+"option3": "XL",
+"sku": "",
+"requires_shipping": true,
+"taxable": true,
+"featured_image": null,
+"available": true,
+"name": "7 Shakra Bracelet - Rubber / Black / XL",
+"public_title": "Rubber / Black / XL",
+"options": [
+"Rubber",
+"Black",
+"XL"
+],
+"price": 4299,
+"weight": 0,
+"compare_at_price": 4499,
+"inventory_management": null,
+"barcode": "",
+"requires_selling_plan": false,
+"selling_plan_allocations": []
+},
+{
+"id": 45387608785152,
+"title": "Rubber / Green / L",
+"option1": "Rubber",
+"option2": "Green",
+"option3": "L",
+"sku": "",
+"requires_shipping": true,
+"taxable": true,
+"featured_image": null,
+"available": true,
+"name": "7 Shakra Bracelet - Rubber / Green / L",
+"public_title": "Rubber / Green / L",
+"options": [
+"Rubber",
+"Green",
+"L"
+],
+"price": 4299,
+"weight": 0,
+"compare_at_price": 4499,
+"inventory_management": null,
+"barcode": "",
+"requires_selling_plan": false,
+"selling_plan_allocations": []
+},
+{
+"id": 45387608817920,
+"title": "Rubber / Green / XL",
+"option1": "Rubber",
+"option2": "Green",
+"option3": "XL",
+"sku": "",
+"requires_shipping": true,
+"taxable": true,
+"featured_image": null,
+"available": true,
+"name": "7 Shakra Bracelet - Rubber / Green / XL",
+"public_title": "Rubber / Green / XL",
+"options": [
+"Rubber",
+"Green",
+"XL"
+],
+"price": 4299,
+"weight": 0,
+"compare_at_price": 4499,
+"inventory_management": null,
+"barcode": "",
+"requires_selling_plan": false,
+"selling_plan_allocations": []
+},
+{
+"id": 45387608850688,
+"title": "Rubber / Red / L",
+"option1": "Rubber",
+"option2": "Red",
+"option3": "L",
+"sku": "",
+"requires_shipping": true,
+"taxable": true,
+"featured_image": null,
+"available": true,
+"name": "7 Shakra Bracelet - Rubber / Red / L",
+"public_title": "Rubber / Red / L",
+"options": [
+"Rubber",
+"Red",
+"L"
+],
+"price": 4299,
+"weight": 0,
+"compare_at_price": 4499,
+"inventory_management": null,
+"barcode": "",
+"requires_selling_plan": false,
+"selling_plan_allocations": []
+},
+{
+"id": 45387608883456,
+"title": "Rubber / Red / XL",
+"option1": "Rubber",
+"option2": "Red",
+"option3": "XL",
+"sku": "",
+"requires_shipping": true,
+"taxable": true,
+"featured_image": null,
+"available": true,
+"name": "7 Shakra Bracelet - Rubber / Red / XL",
+"public_title": "Rubber / Red / XL",
+"options": [
+"Rubber",
+"Red",
+"XL"
+],
+"price": 4299,
+"weight": 0,
+"compare_at_price": 4499,
+"inventory_management": null,
+"barcode": "",
+"requires_selling_plan": false,
+"selling_plan_allocations": []
+},
+{
+"id": 45387608916224,
+"title": "Rubber / White / L",
+"option1": "Rubber",
+"option2": "White",
+"option3": "L",
+"sku": "",
+"requires_shipping": true,
+"taxable": true,
+"featured_image": null,
+"available": true,
+"name": "7 Shakra Bracelet - Rubber / White / L",
+"public_title": "Rubber / White / L",
+"options": [
+"Rubber",
+"White",
+"L"
+],
+"price": 4299,
+"weight": 0,
+"compare_at_price": 4499,
+"inventory_management": null,
+"barcode": "",
+"requires_selling_plan": false,
+"selling_plan_allocations": []
+},
+{
+"id": 45387608948992,
+"title": "Rubber / White / XL",
+"option1": "Rubber",
+"option2": "White",
+"option3": "XL",
+"sku": "",
+"requires_shipping": true,
+"taxable": true,
+"featured_image": null,
+"available": true,
+"name": "7 Shakra Bracelet - Rubber / White / XL",
+"public_title": "Rubber / White / XL",
+"options": [
+"Rubber",
+"White",
+"XL"
+],
+"price": 4299,
+"weight": 0,
+"compare_at_price": 4499,
+"inventory_management": null,
+"barcode": "",
+"requires_selling_plan": false,
+"selling_plan_allocations": []
+}
+],
+"images": [
+"//cdn.shopify.com/s/files/1/0707/1354/0864/files/7-chakra-bracelet_925x_f6b9d552-6950-4b08-87bf-82fd4f0d3641.jpg?v=1717998592",
+"//cdn.shopify.com/s/files/1/0707/1354/0864/files/navy-blue-chakra-bracelet_925x_8b2d20e0-f381-43d4-a5fb-65e9095877e5.jpg?v=1717998592"
+],
+"featured_image": "//cdn.shopify.com/s/files/1/0707/1354/0864/files/7-chakra-bracelet_925x_f6b9d552-6950-4b08-87bf-82fd4f0d3641.jpg?v=1717998592",
+"options": [
+{
+"name": "Material",
+"position": 1,
+"values": [
+"Rubber"
+]
+},
+{
+"name": "Color",
+"position": 2,
+"values": [
+"Blue",
+"Black",
+"Green",
+"Red",
+"White"
+]
+},
+{
+"name": "Size",
+"position": 3,
+"values": [
+"L",
+"XL"
+]
+}
+],
+"url": "/products/chain-bracelet",
+"media": [
+{
+"alt": null,
+"id": 33953370177792,
+"position": 1,
+"preview_image": {
+"aspect_ratio": 0.667,
+"height": 1386,
+"width": 925,
+"src": "https://cdn.shopify.com/s/files/1/0707/1354/0864/files/7-chakra-bracelet_925x_f6b9d552-6950-4b08-87bf-82fd4f0d3641.jpg?v=1717998592"
+},
+"aspect_ratio": 0.667,
+"height": 1386,
+"media_type": "image",
+"src": "https://cdn.shopify.com/s/files/1/0707/1354/0864/files/7-chakra-bracelet_925x_f6b9d552-6950-4b08-87bf-82fd4f0d3641.jpg?v=1717998592",
+"width": 925
+},
+{
+"alt": null,
+"id": 33953370210560,
+"position": 2,
+"preview_image": {
+"aspect_ratio": 1.497,
+"height": 618,
+"width": 925,
+"src": "https://cdn.shopify.com/s/files/1/0707/1354/0864/files/navy-blue-chakra-bracelet_925x_8b2d20e0-f381-43d4-a5fb-65e9095877e5.jpg?v=1717998592"
+},
+"aspect_ratio": 1.497,
+"height": 618,
+"media_type": "image",
+"src": "https://cdn.shopify.com/s/files/1/0707/1354/0864/files/navy-blue-chakra-bracelet_925x_8b2d20e0-f381-43d4-a5fb-65e9095877e5.jpg?v=1717998592",
+"width": 925
+}
+],
+"requires_selling_plan": false,
+"selling_plan_groups": []
+}
