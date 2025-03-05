8e640b749d14580eb34fd53411cc32d689826bed
daiyagopal <gopal@taggbox.com>
2025-03-05 12:59:05 +0530

    update

---
+++ widget/static/js/7789.5a163156.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkembed_widget=self.webpackChunkembed_widget||[]).push([[7789],{90170:(t,e,s)=>{s.r(e),s.d(e,{default:()=>d});var l=s(70579);const d=t=>{var e;let{CardStyle:s,btnClass:d,mode:n}=t;const a={fontFamily:null===s||void 0===s||null===(e=s.style)||void 0===e?void 0:e.font};return(0,l.jsx)("div",{className:`tb_shop_btn_wrap ${d}`,children:(0,l.jsx)("div",{className:"tb_shop_btn"+(n?" tb_shop_btn_light":" tb_shop_btn_dark"),style:a,children:s.shopText})})}}}]);
+//# sourceMappingURL=7789.5a163156.chunk.js.map
