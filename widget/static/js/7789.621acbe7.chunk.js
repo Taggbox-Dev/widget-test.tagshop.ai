96ede7c47c932d0b2388b72c65f1a46e037775c2
daiyagopal <gopal@taggbox.com>
2025-03-25 19:07:08 +0530

    update

---
+++ widget/static/js/7789.621acbe7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkembed_widget=self.webpackChunkembed_widget||[]).push([[7789],{90170:(t,e,s)=>{s.r(e),s.d(e,{default:()=>d});var l=s(70579);const d=t=>{var e;let{CardStyle:s,btnClass:d}=t;const n={fontFamily:null===s||void 0===s||null===(e=s.style)||void 0===e?void 0:e.font};return(0,l.jsx)("div",{className:`tb_shop_btn_wrap ${d}`,children:(0,l.jsxs)("div",{className:"tb_shop_btn"+(s.style.mode?" tb_shop_btn_light":" tb_shop_btn_dark"),style:n,children:["      ",s.shopText]})})}}}]);
+//# sourceMappingURL=7789.621acbe7.chunk.js.map
